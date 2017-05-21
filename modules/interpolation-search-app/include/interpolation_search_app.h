2d87b751963edaf61d7f53069ff2c9261230fd82
IlyaKiparenko <ilya.kiparenko@gmail.com>
2017-05-21 17:02:18 +0400

    Delete comments

--- modules/interpolation-search-app/include/interpolation_search_app.h
+++ modules/interpolation-search-app/include/interpolation_search_app.h
@@ -1,27 +1,3 @@
-/*
-
-// Copyright 2017 Kiparenko Ilya
-
-#ifndef MODULES_INTERPOLATION_SEARCH_APP_INCLUDE_INTERPOLATION_SEARCH_APP_H_
-#define MODULES_INTERPOLATION_SEARCH_APP_INCLUDE_INTERPOLATION_SEARCH_APP_H_
-
-#include <string>
-
-class InterpolationSearchApp {
- public:
-  InterpolationSearchApp();
-  std::string operator() (int argc, const char** argv);
- private:
-  static const char help_message_[] = 
-    "This is a interpolation search app\n"
-    "Use interpolation-search <array> <item>\n"
-    "Where <array> is array of integers, and <item> is what you want to find\n";
-};
-
-#endif  // MODULES_INTERPOLATION_SEARCH_APP_INCLUDE_INTERPOLATION_SEARCH_APP_H_
-
-*/
-
 // Copyright 2017 Kiparenko Ilya
 
 #ifndef MODULES_INTERPOLATION_SEARCH_APP_INCLUDE_INTERPOLATION_SEARCH_APP_H_
